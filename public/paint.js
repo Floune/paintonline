b5a746d8d1fdd440ff7b23bc5473aacdeaf8f2d0
RobinFloune <rfournier@simplonprod.co>
2020-08-31 11:18:50 +0200

    recalibrer

--- public/paint.js
+++ public/paint.js
@@ -6,6 +6,8 @@ const context = canvas.getContext('2d');
 let color = "";
 erase();
 
+let rect = paint.getBoundingClientRect();
+
 socket.on('drawing', function(data){
 	drawLine(context, data.x, data.y, data.x1, data.y1, data.color)
 })
@@ -39,7 +41,7 @@ document.getElementById("pink").addEventListener("click", e => {
 })
 
 paint.addEventListener('mousedown', e => {
-	const rect = paint.getBoundingClientRect();
+	rect = paint.getBoundingClientRect();
 	x = e.clientX - rect.left;
 	y = e.clientY - rect.top;
 	isDrawing = true;
